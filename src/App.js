fd76059a59ce554043e99127b25931b90ec47ab2
TukaramSReddy <tukaramreddy90@gmail.com>
2023-04-19 15:19:53 +0000

    Visit Countries Solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+import CountriesHome from './components/CountriesHome'
+
+function App() {
+  return (
+    <div className="App">
+      <CountriesHome />
+    </div>
+  )
+}
+
+export default App
